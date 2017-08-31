035e56573f7d9c4bb173e35c88f1dce3897859df
Alan Corrales <a154475@dac.unicamp.br>
2017-08-31 11:31:01 -0300

    Update script.sh

--- Projeto 1/script.sh
+++ Projeto 1/script.sh
@@ -9,20 +9,22 @@ hadoop namenode -format
 start-dfs.sh
 
 # Hadoop Prep
-$HADOOP_HOME/bin/hadoop dfs -ls /user/input
-$HADOOP_HOME/bin/hadoop dfs -mkdir /user/input/sertanejo
-$HADOOP_HOME/bin/hadoop dfs -copyFromLocal /home/letras/sertanejo/* /user/input/sertanejo
-$HADOOP_HOME/bin/hadoop dfs -ls /user/input
+$HADOOP_HOME/bin/hadoop fs -ls /user/input
+$HADOOP_HOME/bin/hadoop fs -mkdir /user
+$HADOOP_HOME/bin/hadoop fs -mkdir /user/input
+$HADOOP_HOME/bin/hadoop fs -mkdir /user/input/sertanejo
+$HADOOP_HOME/bin/hadoop fs -copyFromLocal /home/letras/sertanejo/* /user/input/sertanejo
+$HADOOP_HOME/bin/hadoop fs -ls /user/input
 ...
-$HADOOP_HOME/bin/hadoop dfs -mkdir /user/results
+$HADOOP_HOME/bin/hadoop fs -mkdir /user/output
 
 # MapReduce Execution
 $HADOOP_HOME/bin/hadoop jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-2.7.4.jar \ 
 -file /home/hadoop/mapper.py -mapper /home/hadoop/mapper.py \
 -file /home/hadoop/reducer.py -reducer /home/hadoop/reducer.py \
--input /user/input/sertanejo/* -output /user/results
+-input /user/input/sertanejo/* -output /user/output
 
 # Getting Results
-$HADOOP_HOME/bin/hadoop dfs -ls /user/results #Show result files
-$HADOOP_HOME/bin/hadoop dfs -cat /user/results/OUTFILE  # Visualize results
-$HADOOP_HOME/bin/hadoop dfs -get /user/results/ /home/hadoop/ # Get results
+$HADOOP_HOME/bin/hadoop fs -ls /user/output #Show result files
+$HADOOP_HOME/bin/hadoop fs -cat /user/output/OUTFILE  # Visualize results
+$HADOOP_HOME/bin/hadoop fs -get /user/output/ /home/hadoop/ # Get results
