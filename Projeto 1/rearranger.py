a79d1760fb22fcad4ff37409553ba12ad467dbbf
JTStark <joao.baraldi61@gmail.com>
2017-09-05 13:34:10 -0300

    rearranger update

--- Projeto 1/rearranger.py
+++ Projeto 1/rearranger.py
@@ -1,9 +1,22 @@
-filein = open("part-00000", "r")
-fileout = open("out-00000", "w")
+import os as os
 
-for line in filein:
-    out = line.split()
-    fileout.write(out[-1] + " " + out[0] + "\n")
+wordlist = ["para", "de", "com", "por", "em", "a", "o", "as", "os", u"à", u"às",
+    "aos", "e", u"é", u"está", "na", "nas", "no", "nos", "pelo", "pela",
+    "mas", "da", "do", "das", "dos"]
 
-filein.close()
-fileout.close()
+local_dir = os.getcwd()
+home = local_dir + "/Resultados/"
+directory = os.fsencode(home)
+
+for filename in os.listdir(directory):
+    filenamestr = os.fsdecode(filename)
+
+    filein = open(home + filenamestr, 'r')
+    fileout = open(home + filenamestr + "_clean", "w+")
+    for line in filein:
+        out = line.split()
+        if out[0] not in wordlist:
+            fileout.write(out[-1] + " " + out[0] + "\n")
+
+    filein.close()
+    fileout.close()
